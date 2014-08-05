1db9d7527714139a7659165939a1db205576a8ec
Marcos Bérgamo <mbergamo@inviron.com.br>
2014-08-05 10:10:29 -0300

    first commit

--- neuronios/somar.js
+++ neuronios/somar.js
@@ -3,6 +3,7 @@ var actions = ['somar', 'some'];
 module.exports = function(expression){
     if(actions.indexOf(expression[0]) < -1)
         return false;
+
     expression.shift();
     var answer = 0;
     expression.forEach(function(value){
